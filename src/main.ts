1651456c84dfa6542b0eec8ac8acbc00160ace2f
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-06-09 11:25:45 +0200

    Add all routes

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,41 @@ import VueRouter from "vue-router";
 // views
 import Home from "../src/views/Home.vue";
 import ProcessorScheduling from "../src/views/ProcessorScheduling.vue";
+import DiskScheduling from "../src/views/DiskScheduling.vue";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  mode: "history",
   routes: [
-    { path: "/", component: Home, name: "Home" },
+    // { path: "/", component: Home, name: "Home" },
+    { path: "/", redirect: "processor-scheduling", meta: { hidden: true } },
     {
       path: "/processor-scheduling",
       component: ProcessorScheduling,
       name: "Processor scheduling"
+    },
+    {
+      path: "/disk-scheduling",
+      component: DiskScheduling,
+      name: "Disk scheduling"
+    },
+    {
+      path: "/page-replacement",
+      component: Home,
+      name: "Page replacement"
+    },
+    {
+      path: "/frame-allocation",
+      component: Home,
+      name: "Frame allocation"
+    },
+    {
+      path: "/distributed-processor-scheduling",
+      component: Home,
+      name: "Distributed processor scheduling"
     }
   ]
 });
